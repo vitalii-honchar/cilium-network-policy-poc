7fdcc050e131c1bb917e9f2f88a79c9b9f3bd459
Vitalii Honchar <weaxme@gmail.com>
2023-04-15 10:28:01 -0400

    Adjusted code for run an example

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # resilience-testing-example
 This repository contains an example for resilience testing in Go service.
 
-## Run tests
-1. `docker-compose up -d`
-2. `make test`
+## Launch cluster with Cilium network plugin
+1. `minikube start --network-plugin=cni --cni=false`
+2. Install Cilium from [Docs](https://docs.cilium.io/en/stable/gettingstarted/k8s-install-default/)
+3. `cilium install`
+4. `cilium status --wait`
+
+
+## Deploy PoC application
+1. `eval $(minikube docker-env)`
+2. `make dockerbuild`
+3. `kubectl create -f deployments/server.yaml`
+4. `kubectl create -f deployments/worker.yaml`
+5. Inspect pod logs in `k9s`
+
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -25,41 +25,23 @@ func main() {
 	if workerUrl == "" {
 		panic("worker url was missed")
 	}
-	mux := http.NewServeMux()
 
-	client := &http.Client{Transport: &http.Transport{
-		DisableKeepAlives: true,
-	}}
-
-	mux.Handle("/send-work", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	for {
 		req := &WorkRequest{Id: random.Intn(1000), Name: fmt.Sprintf("work-%d", random.Intn(1000))}
 
 		reqBytes, err := json.Marshal(req)
 		if err != nil {
 			log.Printf("can't marshall request: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+			continue
 		}
 
-		_, err = client.Post(workerUrl+"/work", "application/json", bytes.NewBuffer(reqBytes))
+		_, err = http.Post(workerUrl+"/work", "application/json", bytes.NewBuffer(reqBytes))
 
 		if err != nil {
 			log.Printf("error response from worker: %v\n", err)
-
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+			continue
 		}
 
-		log.Printf("sent work from: %s\n", r.Host)
-
-		w.WriteHeader(http.StatusOK)
-	}))
-
-	s := &http.Server{Handler: mux, Addr: fmt.Sprintf(":%d", 8080)}
-
-	log.Printf("starting server on %s", s.Addr)
-	err := s.ListenAndServe()
-	if err != nil && err != http.ErrServerClosed {
-		log.Printf("http server error: %v\n", err)
+		log.Printf("sent work: %+v\n", req)
 	}
 }
